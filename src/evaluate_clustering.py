6576f8f27aa7a460876845e98f5f9cb5c3130caf
milhud <178525554+milhud@users.noreply.github.com>
2025-09-01 21:31:17 -0500

    load and preprocess, then evaluate

---
+++ src/evaluate_clustering.py
@@ -0,0 +1,57 @@
+def evaluate_clustering_detailed(topics, categories): # ground truth categories passed
+    
+    # stats
+    unique_topics = set(topics)
+    clusters = len(unique_topics) # includes outliers
+    outliers = 0
+
+    for topic in topics:
+        if topic == -1:
+            outliers += 1
+    
+    clustered = len(topics) - outliers
+    outlier_percentage = (outliers / len(topics)) * 100
+    clustered_percentage = (clustered / len(topics)) * 100
+    
+    # if all outliers just return with zeroes
+    if clustered is 0:
+        return 0.0, 0.0, 0.0
+    
+    # going to start by eliminating the outliers - first create mask
+    mask = []
+    for topic in topics:
+        if topic != -1:
+            mask.append(True)
+        else:
+            mask.append(False)
+    
+    # filter with mask - keep non outliers
+    filtered_topics = []
+    for i in range(len(topics)):
+        topic = topics[i]
+        keep = mask[i]
+        if keep:
+            filtered_topics.append(topic)
+    
+    # now the same for the categories
+    filtered_categories = []
+    for i in range(len(categories)):
+        category = categories[i]
+        keep = mask[i]
+        if keep:
+            filtered_categories.append(category)
+    
+    # now to encode categories before calculating score
+    from sklearn.preprocessing import LabelEncoder
+    encoder = LabelEncoder()
+    true_labels = encoder.fit_transform(filtered_categories)
+    
+    # calculate score
+    from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
+    ari = adjusted_rand_score(true_labels, filtered_topics)
+    nmi = normalized_mutual_info_score(true_labels, filtered_topics)
+    
+    # clustering success rate
+    clustering_success_rate = clustered / len(topics)
+    
+    return ari, nmi, clustering_success_rate
--- src/load_data.py
+++ src/load_data.py
@@ -9,11 +9,20 @@ def loadPreprocess():
     answers = dataframe["answer"].tolist()
     category = dataframe["category"].tolist()
 
-    # now clean questions, answers
+    # now clean questions, answers and combine
     cleaned_questions = []
     for question in questions:
         cleaned_questions.append(clean(question))
     
     cleaned_answers = []
     for answer in answers:
+        cleaned_answers.append(clean(answer))
+
+    combined = []
+    for i in range(len(cleaned_questions)):
+        question = cleaned_questions[i] # should be same size 
+        answer = cleaned_answers[i]
+        combined.append(f"{question} {answer}")
+    
+    return combined, category, cleaned_questions # latter two for ground truth and clustering on questions alone
 
